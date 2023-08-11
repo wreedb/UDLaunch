bc328984ccac68bda1067c6e8686c15ba70c4c9f
Will Reed <wreedb@skiff.com>
2023-08-11 16:56:53 -0500

    fix: make toml config variable optional

--- src/main.v
+++ src/main.v
@@ -1,22 +1,21 @@
 module main
 
 import os
-import toml
 import term
+import toml
 
 struct Daemon
 {
-	mut: 
+    mut:
         name string
-	    restart bool
-	    args []string
-	    abs_path string
+        restart bool
+        args []string
+        abs_path string
         setenvs map[string]string
         unsetenvs []string
 }
 
 const u_env := os.environ()
-
 
 fn main()
 {
@@ -26,7 +25,7 @@ fn main()
     mut config_dir  := conf[0]
     mut config_file := conf[1]
 
-    os.ensure_folder_is_writable(config_dir) or { errlog("Couldn't find your '${config_dir}' directory!") }
+    os.ensure_folder_is_writable(config_dir) or { panic("Couldn't find your '${config_dir}' directory!") }
 
     if os.exists(config_file) == true
     {
@@ -36,119 +35,124 @@ fn main()
         {
             run_daemon(mut udaemon_array[index])
         }
-    
+        
+        log_ok("processes launched successfully")
+
     }
 
     else
     {
-        errlog("Couldn't find your configuration file '${config_file}' ")
+        log_err("Couldn't find your configuration file '${config_file}' ")
     }
 
 }
 
 fn check_running(name string) bool
 {
-	if os.execute('pgrep ${name}').exit_code == 0 { return true } else { return false }
+    if os.execute('pgrep ${name}').exit_code == 0 { return true } else { return false }
 }
 
 fn get_daemon_pids(daemon string) []string
 {
     mut pgrep := os.execute('pgrep ${daemon}')
 
-    match pgrep.exit_code
-    {
-        0
-        {
-            mut pids := pgrep.output.split('\n')
-            pids.delete_last()
-            return pids
-        }
+    if pgrep.exit_code != 0 { return [] }
 
-        else { return [] }
-    }    
+    mut pids := pgrep.output.split('\n')
+    pids.delete_last()
+    return pids
+
 }
 
 fn run_daemon(mut daemon Daemon)
 {
 
-	mut daemon_process := os.new_process(daemon.abs_path)
+    mut daemon_process := os.new_process(daemon.abs_path)
     
     daemon_process.set_redirect_stdio()
     
     // TODO allow for user to force stdio to be shown
-    
-    /* --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- */
 
+    /* unset env vars */
     if daemon.unsetenvs.len > 0
     {
         proc_unsetenvs(mut daemon_process, daemon.unsetenvs)
     }
-    
-    /* --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- */
-    
+
+    // set env vars
     if daemon.setenvs.len > 0
     {
         proc_setenvs(mut daemon_process, daemon)
     }
     
-    /* --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- */
-    
+    // append arguments
     if daemon.args.len > 0
     {
         proc_setargs(mut daemon_process, daemon.args)
     }
     
-    /* --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- */
-    
+    // kill it
     if daemon.restart
     {
         kill_daemon(get_daemon_pids(daemon.name))
+        log_info("found ${daemon.name} runnning, killing it")
     }
 
-    /* --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- */
-
+    // Check if it's running to skip or continue
     if check_running(daemon.name)
     {
-        println(oklog("${daemon.name} is already running"))
+        log_ok("${daemon.name} is already running")
         return
     }
     
     // finally after all that, run it
     daemon_process.run()
-    return
+    log_info("launching ${term.italic(daemon.name)}")
 
+    return
 
 }
 
-
 fn kill_daemon(pids []string)
 {
-    for idx in 0 .. pids.len
+    for i in 0 .. pids.len
     {
-		os.execute('kill ${pids[idx]}')
-	}
+        os.execute('kill ${pids[i]}')
+    }
 }
 
 fn read_toml_config(path string) []Daemon
 {
+
     mut parsed_config := toml.parse_file(path) or
     {
-        panic('Could not find test.toml!')
+        panic('Could not find ~/.config/udlaunch/config.toml')
     }
 
     mut num_daemons := parsed_config.to_any().array().len
     mut daemon_array := []Daemon {}
-  
-    for idx in 0 .. num_daemons
+    
+
+    for i in 0 .. num_daemons
     {
+
+        mut current_daemon := parsed_config.to_any().array()[i]
         mut this := Daemon {}
-        this.name      = parsed_config.to_any().array()[idx].value('name').string()
-        this.restart   = parsed_config.to_any().array()[idx].value('restart').bool()
-        this.args      = parsed_config.to_any().array()[idx].value('args').array().as_strings()
-        this.setenvs   = parsed_config.to_any().array()[idx].value('setenvs').as_map().as_strings()
-        this.unsetenvs = parsed_config.to_any().array()[idx].value('unsetenvs').array().as_strings()
-        this.abs_path  = os.find_abs_path_of_executable(this.name) or { panic("Couldn't find the executable for ${this.name} in your PATH!") }
+
+        this.name = current_daemon.value('name').string()
+
+        if val := current_daemon.value_opt('restart')   { this.restart   = val.bool()                } else { this.restart   = false }
+        if val := current_daemon.value_opt('args')      { this.args      = val.array().as_strings()  } else { this.args      = []    }
+        if val := current_daemon.value_opt('setenvs')   { this.setenvs   = val.as_map().as_strings() } else { this.setenvs   = {}    }
+        if val := current_daemon.value_opt('unsetenvs') { this.unsetenvs = val.array().as_strings()  } else { this.unsetenvs = []    }
+
+        this.abs_path = os.find_abs_path_of_executable(this.name) or
+        {
+            panic ("Couldn't find the executable for ${this.name} in your PATH!")
+        }
+        
         daemon_array << this
+    
     }
 
     return daemon_array
@@ -165,11 +169,10 @@ fn get_home_files() []string
     if test_confdir == '' { xdg_home = '${homedir}/.config' }
     else { xdg_home = os.config_dir() or { panic('Internal error finding configuration directory') } }
 
-    mut config_dir  := '${xdg_home}/udlaunch'
+    mut config_dir := '${xdg_home}/udlaunch'
     mut config_file := '${config_dir}/config.toml'
 
     return [config_dir, config_file]
-
 }
 
 fn proc_setargs(mut proc os.Process, args_to_set []string) os.Process
@@ -180,9 +183,8 @@ fn proc_setargs(mut proc os.Process, args_to_set []string) os.Process
 
 fn proc_unsetenvs(mut proc os.Process, envs_to_unset []string) os.Process
 {
- 
     mut custom_env := u_env.clone()
-    
+
     for i in 0 .. envs_to_unset.len
     {
         mut this := envs_to_unset[i]
@@ -192,19 +194,18 @@ fn proc_unsetenvs(mut proc os.Process, envs_to_unset []string) os.Process
     proc.set_environment(custom_env)
     
     return proc
-
 }
 
-fn proc_setenvs(mut proc os.Process, dmn Daemon) os.Process
+fn proc_setenvs(mut proc os.Process, daemon Daemon) os.Process
 {
 
-    mut set_keys := dmn.setenvs.keys()
-    mut set_vals := dmn.setenvs.values()
+    mut set_keys := daemon.setenvs.keys()
+    mut set_vals := daemon.setenvs.values()
     
     // if the env was already manually set
     if proc.env_is_custom
     {
-        for i in 0 .. dmn.setenvs.len
+        for i in 0 .. daemon.setenvs.len
         {
             proc.env << '${set_keys[i]}=${set_vals[i]}'
         }
@@ -213,7 +214,7 @@ fn proc_setenvs(mut proc os.Process, dmn Daemon) os.Process
     else
     {
         mut custom_env := u_env.clone()
-        for i in 0 .. dmn.setenvs.len
+        for i in 0 .. daemon.setenvs.len
         {
             custom_env[set_keys[i]] = set_vals[i]
         }
@@ -224,12 +225,20 @@ fn proc_setenvs(mut proc os.Process, dmn Daemon) os.Process
 
 }
 
-fn errlog(err string) string
+fn log_ok(msg string)
 {
-    return term.red(term.bold('UDLAUNCH: ${term.white(err)}'))
+    mut msg_white := term.white(msg)
+    println( term.green( term.bold('UDLAUNCH: ${msg_white}') ) )
 }
 
-fn oklog (msg string) string
+fn log_info(msg string)
 {
-    return term.blue(term.bold('UDLAUNCH: ${term.white(msg)}'))
+    mut msg_white := term.white(msg)
+    println( term.blue( term.bold('UDLAUNCH: ${msg_white}') ) )
+}
+
+fn log_err(msg string)
+{
+    mut msg_white := term.white(msg)
+    println( term.red( term.bold('UDLAUNCH: ${msg_white}') ) )
 }
